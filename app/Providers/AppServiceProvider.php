742e1f5a69e26a74ea19fd60c21a9bee942c6070
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2017-04-07 18:03:50 +0530

    Setting default string length for index keys for MySQL Version < 5.7.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // When migrating the database on MySQL below v5.7.7 versions
+        // Following error occurs:
+        // SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;
+        // max key length is 767 bytes.
+
+        // Laravel 5.4 made a change to the default database character set,
+        // and it’s now utf8mb4 which includes support for storing emojis.
+        // This only affects new applications and as long as you are running
+        // MySQL v5.7.7 and higher you do not need to do anything.
+        Schema::defaultStringLength(191);
     }
 
     /**
