da0486efff910d56e3c296852932f22a1078a107
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2017-04-07 19:36:04 +0530

    App config

--- config/database.php
+++ config/database.php
@@ -95,13 +95,26 @@ return [
 
     'redis' => [
 
-        'client' => 'predis',
+'client' => 'predis',
+        'cluster' => false,
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
+        ],
+        'for-cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+        'for-session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 2,
         ],
 
     ],
